f732335239a744fb04f0659c20abd282a35697aa
wataengen <wata69naka@gmail.com>
2023-11-23 10:50:38 +0900

    sample2追加2

---
+++ Ruby/sample2/endless_method.rb
@@ -0,0 +1,20 @@
+def greet1
+    'Hello!, 1'
+end
+
+def greet2 = 'Hello!, 2'
+
+puts greet1
+puts greet2
+
+def add1(a, b)
+    a + b
+end
+
+def add2(a, b) = a + b
+
+puts add1(1, 2)
+puts add2(1, 2)
+
+puts 'Hello'.length
+puts 'Hello'.size
