720df32a7939d736b6f9f37b6a1ad84580965184
wataengen <wata69naka@gmail.com>
2023-12-04 14:34:55 +0900

    ブロックフォルダにファイルを追加

---
+++ Ruby/block/map_collect.rb
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5]
+space = ""
+
+new_numbers1 = []
+numbers.each {|n| new_numbers1 << n * 10}
+print "new_number1", new_numbers1
+p space
+
+new_numbers2 = numbers.map {|n| n * 10}
+print "new_number2", new_numbers2
